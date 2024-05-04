4a9a68e6f6e149ad9631a5166ab16c56db7fc058
Miguel Cortes <miguel.cortes@ias.com.co>
2024-05-04 07:45:23 -0500

    clear workspace

--- src/main/java/co/com/tkl/app/repositories/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package co.com.tkl.app.repositories;
-
-import co.com.tkl.app.entities.Invoice;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
-}
